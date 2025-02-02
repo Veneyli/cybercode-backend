594c8f6e0e03f2a156d1381b73d95d326821abda
Kulikova Darya <daryakulikova2206@mail.ru>
2025-02-02 23:16:59 +0300

    changes main 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://10.0.1.114:3000',
+      'http://cybercode.veney.tech',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
