2020a58a258fc3aab5eb05d4676356f092435868
Kulikova Darya <daryakulikova2206@mail.ru>
2025-02-03 00:54:19 +0300

    aaaa

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,12 @@ async function bootstrap() {
   const port = process.env.PORT || 4000;
 
   await app.listen(port);
-  // .then(() => {
-  //   console.log(`Сервер запущен на http://localhost:${port}`);
-  // })
-  // .catch((error) => {
-  //   console.error('Ошибка при запуске сервера:', error);
-  // });
+  .then(() => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+  })
+  .catch((error) => {
+    console.error('Ошибка при запуске сервера:', error);
+  });
 }
 
 bootstrap();
